6ae60e13083c81bb8112bbb3ab4bb78e91398ac6
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-01-22 13:04:51 +0100

    asd

--- e3.py
+++ e3.py
@@ -5,3 +5,20 @@
     Descripció:
     Programa de traducció d'insults.
 """
+
+insultsDiccionari = {
+    'CAT': ["CARALLOT", "MOCOS", "MALPARIT"],
+    'ESP': ["CARAROLLO", "MOCOSO", "MALPARIDO"],
+    'ENG': ["HECK", "BRAT", "BASTARD"],
+    'KLI': ["QARDANG", "yIv", "marqeq"]
+}
+
+print("Introdueix l'insult en majúscules\n")
+insult = input("")
+
+if insult == insultsDiccionari:
+
+
+# print(['ESP'][insulto])
+# print(['ENG'][insulto])
+# print(['KLI'][insulto])
