07fbb68be9191670e0eeb3b1d5db4df0ceeb654a
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-01-22 11:37:29 +0100

    asd

---
+++ e1.py
@@ -0,0 +1,7 @@
+"""
+    Noms: Daniel Arco, Izan Arnáiz.
+    Grup: ASIXc1A
+    Data: 22/01/2024
+    Descripció:
+    Programa que calculi la temperatura del mar
+"""
---
+++ e2.py
@@ -0,0 +1,7 @@
+"""
+    Noms: Daniel Arco, Izan Arnáiz.
+    Grup: ASIXc1A
+    Data: 22/01/2024
+    Descripció:
+    Lista de 100 nombres aleatorios entre 1 y 50.
+"""
