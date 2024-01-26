64e50c3f13e6f6ec5ffee3cf4635b6db9e55ac37
Izan <izan.arnaiz.7e5@itb.cat>
2024-01-26 17:20:07 +0100

    e3Hecho

--- e3na.py
+++ e3na.py
@@ -6,3 +6,18 @@
     Programa que determina si un valor es múltiple de 2 i/o si es múltiple de 5..
 """
 
+try:
+    valor = int(input(""))
+
+    if valor % 2 == 0 and valor % 5 == 0:
+        print("Múltiple de 2 i 5")
+    elif valor % 2 == 0:
+        print("Múltiple de 2")
+    elif valor % 5 == 0:
+        print("Múltiple de 5")
+    else:
+        print("Error, el valor introduït no es múltiple de 2 i/o 5\n")
+except ValueError:
+    print("Error, introduce un valor entero positivo.")
+finally:
+    print("\nPrograma terminado")
